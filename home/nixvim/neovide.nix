18a02feb7b0818edbbf9ebb288eb54cb446cb0dd
Avery Rios <avery-dd70@outlook.com>
2024-01-08 15:20:32 +0800

    [home] nixvim/neovide: fix font size

--- home/nixvim/neovide.nix
+++ home/nixvim/neovide.nix
@@ -5,6 +5,7 @@ let
         neovide_cursor_animation_length = 0;
         neovide_transparency = 0.8;
       };
+      options = { guifont = "Cascadia Code PL:h10"; };
     };
 
     home.packages = [ pkgs.neovide ];
