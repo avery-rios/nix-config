194c66df73e9b1bad66d59efd54644b83cad9b5d
Avery Rios <avery-dd70@outlook.com>
2024-01-21 10:08:29 +0800

    [home] gpg: use default config for agent

--- home/gpg.nix
+++ home/gpg.nix
@@ -1,8 +1,4 @@
-{ pkgs, ... }: {
+{ ... }: {
   programs.gpg = { enable = true; };
-  services.gpg-agent = {
-    enable = true;
-    pinentryFlavor = "tty";
-  };
-  home.sessionVariables = { GPG_TTY = "$(tty)"; };
+  services.gpg-agent = { enable = true; };
 }
