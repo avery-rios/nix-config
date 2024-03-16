a6ecf5cb9b2a1884a21ff5f50d3d7b5e0ea6675e
Avery Rios <avery-dd70@outlook.com>
2024-03-16 23:14:51 +0800

    [module] dev/haskell: add package doc

--- modules/develop/haskell.nix
+++ modules/develop/haskell.nix
@@ -22,7 +22,13 @@
         browser = {
           firefox = {
             enable = mkEnableOption "Haskell doc";
-            bookmarks.ghc.enable = options.mkDisableOption "GHC document";
+            bookmarks = {
+              ghc.enable = options.mkDisableOption "GHC document";
+              packages = mkOption {
+                type = types.listOf types.package;
+                default = [ ];
+              };
+            };
           };
         };
       };
@@ -33,11 +39,21 @@
       home.packages = lib.mkIf cfg.env.cabal.enable
         (with pkgs; [ ghc cabal-install haskell-language-server ]);
 
-      programs.firefox.policies = lib.mkIf cfg.browser.firefox.enable {
-        ManagedBookmarks = lib.mkIf cfg.browser.firefox.bookmarks.ghc.enable [{
-          name = "GHC Documentation";
-          url = "${pkgs.ghc.doc}/share/doc/ghc/html/index.html";
-        }];
+      programs.firefox.policies = let ffCfg = cfg.browser.firefox;
+      in lib.mkIf ffCfg.enable {
+        ManagedBookmarks = lib.mkMerge [
+          (lib.mkIf ffCfg.bookmarks.ghc.enable [{
+            name = "GHC Documentation";
+            url = "${pkgs.ghc.doc}/share/doc/ghc/html/index.html";
+          }])
+          [{
+            name = "Haskell packages";
+            children = builtins.map (p: {
+              inherit (p) name;
+              url = "${p.doc}/share/doc/${p.name}/html/index.html";
+            }) ffCfg.bookmarks.packages;
+          }]
+        ];
       };
 
       programs.vscode = lib.mkIf cfg.editor.vscode.enable {
